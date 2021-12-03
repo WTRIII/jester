ae949cd9c2a4c3dac91d2e01c6731cc971a1acde
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-12-03 11:53:24 -0600

    added client and front end folders/files

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+
+// =========change======================
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
