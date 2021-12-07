92c77c90975cb1159727be4b075fd00e0cd35722
WTRIII <thomren3@gmail.com>
2021-12-06 19:36:18 -0600

    updated seed script. removed createdBy from jestseeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,18 @@
 const db = require('../config/connection');
-const { Task } = require('../models');
+const { Task, Jest, User } = require('../models');
 const taskSeeds = require('./taskSeeds.json');
+const userSeeds = require('./userSeeds.json');
+const jestSeeds = require('./jestSeeds.json');
 
 db.once('open', async () => {
   try {
     await Task.deleteMany({});
+    await Jest.deleteMany({});
+    await User.deleteMany({});
     await Task.create(taskSeeds);
+    await Jest.create(jestSeeds);
+    await User.create(userSeeds);
+
   } catch (err) {
     console.error(err);
     process.exit(1);
